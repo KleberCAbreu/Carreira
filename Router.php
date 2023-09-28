e72a839eedcf65375ffc49d71be6bdc153497aa9
Kleber <kleber.abreu@etec.sp.gov.br>
2023-09-28 11:32:42 -0300

    Enter

--- Router.php
+++ Router.php
@@ -10,10 +10,18 @@ class Router extends RouterSwitch
         $route = substr($requestUri, 1);
         $route = str_replace("carreira/", "", $route);
 
+        error_log("Rota Requisitada:$route",0);
+        $parts = explode("/",$route);
+        error_log("Partes de requisição: ".print_r($parts,true),0);
+
+        $page = $parts[0];
+
+        $operation = count($parts)>1?$parts[1]:null;
+
         if ($route == '') {
             $this->home();
         } else {
-            $this->$route();
+            $this->$page($operation);
         }
     }
 }
